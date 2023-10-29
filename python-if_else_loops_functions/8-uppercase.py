6d8184b020e7e0e5856176a75de9987f90848669
Frandy Slueue <frandy4ever@gmail.com>
2023-10-29 07:36:07 -0500

    Update string to uppercase letters

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Update string to uppercase letters
+def uppercase(str):
+    """
+    Convert a string to uppercase, leaving non-alphabetical characters unchanged.
+
+    Parameters:
+    s (str): The input string to be converted to uppercase.
+
+    Returns:
+    str: The input string with all alphabetical characters converted to uppercase.
+    """
+    for letter in str:
+        if 'a' <= letter <= 'z':
+            char = ord(letter) - 32
+            print(chr(char), end='')
+        else:
+            print(letter, end='')
