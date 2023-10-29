c8512d80fec6f2c7bdbf6181ec789e556ac35977
Frandy Slueue <frandy4ever@gmail.com>
2023-10-29 08:09:43 -0500

    Function that returns the sum of two integers

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# Function that returns the sum of two integers 
+def add(a, b):
+    """
+    Add two numbers and return the result.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The sum of the two input numbers.
+    """
+    result = int(a) + int(b)
+    return result
