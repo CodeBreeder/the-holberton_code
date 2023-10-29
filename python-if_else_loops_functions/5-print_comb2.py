90dd271760538f71b6c80767e57f08a968519cc9
Frandy Slueue <frandy4ever@gmail.com>
2023-10-29 05:12:33 -0500

    Print the combination of two digits number

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 # Print the combination of two digits number
 for number in range(0, 100):
-    for inner_number in range(number):
-        if inner_number != 8 and number != 9:
-            print(inner_number)
+    if number != 99:
+        print("{:02}".format(number), end=',')
+    else:
+        print("{}".format(number), end='')
