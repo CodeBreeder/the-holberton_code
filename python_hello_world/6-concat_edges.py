9353505ff46a3dafa934ec0e4cbbcea64e7e0a60
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-29 00:18:12 -0500

    String concat and slicing

---
+++ python_hello_world/6-concat_edges.py
@@ -0,0 +1,6 @@
+# String concat and slicing
+str = "Python is an interpreted, interactive, object-oriented programming\
+    language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str = f"{str[39:66]} {str[-22:-18]} {str[:6]}"
+print(str)
