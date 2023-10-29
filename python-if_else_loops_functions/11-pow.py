ed4d324a5688aefa3b0d393c80082a3ccabddbe3
Frandy Slueue <frandy4ever@gmail.com>
2023-10-29 09:16:44 -0500

    Function that computes and returns a to the power of b

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+
+# Function that computes and returns a to the power of b
+def power(a, b):
+    """
+    Calculate the result of raising a number to a given exponent.
+
+    Parameters:
+    base (int or float): The base number.
+    exponent (int or float): The exponent to which the base is raised.
+
+    Returns:
+    int or float: The result of base raised to the power of the exponent.
+    """
+    result = a ** b
+    return result
