e4c0bcb76c1979a8c5c059462dd3b67440c5ff7f
Frandy Slueue <frandy4ever@gmail.com>
2023-10-30 00:01:45 -0500

    update

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This code imports a function 'add' from the 'add_0' module and 
+demonstrates its usage by adding two numbers and printing the result.
+
+Usage:
+    - The 'add' function is imported from the 'add_0' module.
+    - Two variables 'a' and 'b' are initialized with the values 1 
+    and 2, respectively.
+    - The 'add' function is called with arguments 1 and 2, 
+    which adds the two numbers together.
+    - The result of the addition is printed to the console.
+
+Example:
+    If 'add(1, 2)' is called, it will print '3' 
+    to the console, which is the sum of 1 and 2.
+"""
+from add_0 import add
+
+a = 1
+b = 2
+
+print(add(1, 2))
